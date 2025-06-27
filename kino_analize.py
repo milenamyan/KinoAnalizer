a4536f609ee60038a00c7a0d903f7a4380112881
milenamyan <milena.mehrabyan2005@gmail.com>
2025-06-28 03:52:39 +0400

    Adding files

---
+++ kino_analize.py
@@ -0,0 +1,50 @@
+import json
+PATH = "imdb_top_999.json" # put the path to your file here
+
+with open(PATH, "r") as f:
+    data = json.load(f)
+
+TO_DELETE = ["No_of_Votes", "Overview", "Meta_score"]
+
+for kino in data.keys():
+    for k in TO_DELETE:
+        del data[kino][k]
+
+    data[kino]["Name"] = kino
+
+class Movie:
+    Released_Year = None
+    Runtime = None
+    Genre = None
+    IMDB_Rating = None
+    Director = None
+    Star1 = None
+    Star2 = None
+    Star3 = None
+    Star4 = None
+    Gross = None
+    Name = None
+
+    def __init__(self, R_Y, RT, G, S1, S2, S3, S4): #, I_R, D, Gr, N
+        self.Released_Year = R_Y
+        self.Runtime = RT
+        self.Genre = G
+        self.Star1 = S1
+        self.Star2 = S2
+        self.Star3 = S3
+        self.Star4 = S4
+
+
+    @classmethod
+    def WithName(cls,name):
+        R_Y = data[name]["Released_Year"]
+        RT = data[name]["Runtime"]
+        G = data[name]['Genre']
+        S1 = data[name]["Star1"]
+        S2 = data[name]["Star2"]
+        S3 = data[name]["Star3"]
+        S4 = data[name]["Star4"]
+        return cls(R_Y, RT, G, S1, S2, S3, S4)
+
+m = Movie.WithName("The Shawshank Redemption")
+print(m.Star1)
