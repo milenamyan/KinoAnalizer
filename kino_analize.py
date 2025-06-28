1d9cea94a6233a6e2b65a097d1ddfccc4e1324e2
milenamyan <milena.mehrabyan2005@gmail.com>
2025-06-28 04:40:13 +0400

    adding AboutFilm methot

--- kino_analize.py
+++ kino_analize.py
@@ -25,14 +25,18 @@ class Movie:
     Gross = None
     Name = None
 
-    def __init__(self, R_Y = None, RT = None, G = None, S1 = None, S2 = None, S3 = None, S4 = None): #, I_R, D, Gr, N
+    def __init__(self, R_Y = None, RT = None, G = None, I_R = None, D = None, S1 = None, S2 = None, S3 = None, S4 = None, Gr = None, N = None): 
         self.Released_Year = R_Y
         self.Runtime = RT
         self.Genre = G
+        self.IMDB_Rating = I_R
+        self.Director = D
         self.Star1 = S1
         self.Star2 = S2
         self.Star3 = S3
         self.Star4 = S4
+        self.Gross = Gr
+        self.Name = N
 
 
     @classmethod
@@ -40,11 +44,15 @@ class Movie:
         R_Y = data[name]["Released_Year"]
         RT = data[name]["Runtime"]
         G = data[name]['Genre']
+        I_R = data[name]['IMDB_Rating']
+        D = data[name]['Director']
         S1 = data[name]["Star1"]
         S2 = data[name]["Star2"]
         S3 = data[name]["Star3"]
         S4 = data[name]["Star4"]
-        return cls(R_Y, RT, G, S1, S2, S3, S4)
+        Gr = data[name]['Gross']
+        N = name
+        return cls(R_Y, RT, G, I_R, D, S1, S2, S3, S4, Gr, N)
     
     def ReturnAct(self):
         print(', '.join([self.Star1, self.Star2,self.Star3,self.Star4]))
@@ -60,6 +68,16 @@ class Movie:
     def DurationByHours(self):
         hours = int((self.Runtime.split())[0]) / 60
         print(round(hours,1))
+    
+    def AboutFilm(self):
+        text = f"""«{self.Name}» ֆիլմում խաղացել են՝ {data[self.Name]['Star1']}, {data[self.Name]['Star2']} և ուրիշներ։ \
+Ֆիլմի ռեժիսոր՝ {data[self.Name]['Director']}։ \nՏևում է {data[self.Name]['Runtime']} րոպե։ \
+Ժանրը՝ {data[self.Name]['Genre']}։ Թողարկվել է {data[self.Name]['Released_Year']} թվականին։ \
+Միջին ռեյտինգ՝ {data[self.Name]['IMDB_Rating']} IMDb-ում։ \
+Բյուջեն՝ {data[self.Name]['Gross']} դոլար։  
+        """
+
+        print(text)
 
 m = Movie.WithName("The Shawshank Redemption")
-m.DurationByHours()
+m.AboutFilm()
